e81287ce56ffeee4af90f84f7fbcc2c65788fcd0
jjling <jjling2011@gmail.com>
2019-04-27 09:58:02 +0800

    move button left in article editor

--- src/web/js/src/02function.js
+++ src/web/js/src/02function.js
@@ -16,6 +16,7 @@ sip.f.add_frame = function (frame_id, content, tag, style) {
     }
     return Mustache.render(cardjs.lib.load_html(frame_id), d);
 };
+
 sip.f.sort_filekeys = function (a, b) {
     if (a.length <= 5) {
         a = a.substr(0, 4) + '0' + a.substr(-1, 1);
@@ -25,44 +26,46 @@ sip.f.sort_filekeys = function (a, b) {
     }
     return (b - a);
 };
+
 sip.f.patch_date = function (d) {
     if (d.length <= 5) {
         return d.substr(0, 4) + '0' + d.substr(-1, 1);
     }
     return d;
 };
+
 sip.f.filter_json = function (e) {
 
     //id userid type title content tag ctime mtime name lock top
 
     var f = {
         msg: function (d) {
-            return(filterXSS(d));
+            return (filterXSS(d));
         },
         title: function (d) {
-            return(filterXSS(cardjs.lib.decode_utf8(d)));
+            return (filterXSS(cardjs.lib.decode_utf8(d)));
         },
         mtime: function (d) {
             //console.log('mtime:'+d);
-            return(cardjs.lib.getYMD(d));
+            return (cardjs.lib.getYMD(d));
         },
         ctime: function (d) {
-            return(cardjs.lib.getYMD(d));
+            return (cardjs.lib.getYMD(d));
         },
         type: function (d) {
-            return(sip.uset.atypes[(d < sip.uset.atypes.length ? d : 0)]);
+            return (sip.uset.atypes[(d < sip.uset.atypes.length ? d : 0)]);
         },
         name: function (d) {
-            return(filterXSS(d));
+            return (filterXSS(d));
         },
         content: function (d) {
-            return(filterXSS(cardjs.lib.decode_utf8(d)));
+            return (filterXSS(cardjs.lib.decode_utf8(d)));
         },
         top: function (d) {
-            return(d === 0 ? false : true);
+            return (d === 0 ? false : true);
         },
         lock: function (d) {
-            return(d === 0 ? false : true);
+            return (d === 0 ? false : true);
         }
     };
 
